455d8f0817d28bfe055ff51a034d39f23a8bd365
codeKing <mukamimuchiri822@gmail.com>
2019-07-12 15:17:03 +0300

    configured callbacks emitters

--- tests/apiTest/c2bRegister.js
+++ tests/apiTest/c2bRegister.js
@@ -1,14 +1,16 @@
 const mpesaInstance = require("../helpers/instance");
 const app = require("../helpers/server");
 const callBackUrls = require("../helpers/ngrok.js");
+const emitter = require("../helpers/callbacksemitter");
 const ngrok = require("ngrok");
 async function registerURL() {
 	try {
 		console.log("setting up callback server");
 		await ngrok.disconnect();
 		global.NGROK_URL = await callBackUrls(app);
+
 		console.log(`connected to: ${global.NGROK_URL}`);
-		let res = await mpesaInstance.c2bRegister(`${NGROK_URL}/c2b/validation_url`, `${NGROK_URL}/c2b/confirmation`);
+		let res = await mpesaInstance.c2bRegister(`${NGROK_URL}/c2b/confirmation`, `${NGROK_URL}/c2b/validation_url`);
 		console.log(res.data);
 	} catch (error) {
 		console.log(error);
@@ -29,8 +31,12 @@ async function simulate() {
 
 		console.log(res.data);
 	} catch (error) {
-		console.log(`erroe: ${error}`);
+		console.log(`error: ${error}`);
 	}
+	emitter.on("c2bSuccessCallback", function(payload) {
+		console.log(payload);
+		return payload;
+	});
 }
 
 simulate();
--- tests/helpers/server.js
+++ tests/helpers/server.js
@@ -8,19 +8,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	emitter.emit("hello", "From CallbacksEmitter");
-	res.send("Hello World!");
+	res.send("Hello Nazarene");
 });
 // create our webhook endpoint to recive webhooks from Safaricom
-app.post("/c2b/confirmation", (req, res) => {
+app.post("/c2b/validation_url", (req, res) => {
 	res.json({
-		ResponseCode: "00000000",
+		ResponseCode: 0,
 		ResponseDesc: "success",
 	});
 });
-app.post("/c2b/validation_url", (req, res) => {
-	console.log("c2bSuccessCallback", req.body);
+app.post("/c2b/confirmation", (req, res) => {
+	emitter.emit("c2bSuccessCallback", req.body);
 	res.json({
-		ResponseCode: "00000000",
+		ResponseCode: 0,
 		ResponseDesc: "success",
 	});
 });
